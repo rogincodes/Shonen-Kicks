e4c4c49f648eacfeaa1d82b2a1864acbf1bf1c32
rogincodes <cartallarogin@gmail.com>
2025-04-24 09:59:39 +0800

    added a separate component for search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import "../styles.css";
 import { useState, useContext } from "react";
 import SelectedShoeContext from "../context/SelectedShoeContext";
-import ShoeCard from "./ShoeCard";
+import Search from "./Search";
 
 export default function Header({ shoes, category }) {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -9,7 +9,6 @@ export default function Header({ shoes, category }) {
   const [subMenu, setSubMenu] = useState("");
   const [searchIsOpen, setSearchIsOpen] = useState(false);
   const { setSelectedShoe } = useContext(SelectedShoeContext);
-  const [searchTerm, setSearchTerm] = useState("");
 
   const toggleMenu = () => {
     setMenuIsOpen(!menuIsOpen);
@@ -33,26 +32,6 @@ export default function Header({ shoes, category }) {
     setSubMenuIsOpen(!subMenuIsOpen);
   };
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  // This function checks if the shoe name or anime name matches the search term
-  const matchesSearchTerm = (shoe, searchTerm) => {
-    return (
-      shoe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      shoe.anime.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  };
-
-  // This variable holds the filtered shoes based on the search term
-  const filteredShoes = shoes.filter((shoe) => {
-    return matchesSearchTerm(shoe, searchTerm);
-  });
-
-  // Used to limit the number of results shown in the search results
-  const firstSixResults = filteredShoes.slice(0, 8);
-
   return (
     <div>
       <div className="header">
@@ -63,7 +42,7 @@ export default function Header({ shoes, category }) {
           </a>
         </div>
 
-        {/* TOP BUTTONS */}
+        {/* HEADER BUTTONS */}
         <div className="top-buttons">
           <button className="icons" onClick={toggleSearch}>
             <img src="icons/search-all.png" alt="Search" />
@@ -211,37 +190,11 @@ export default function Header({ shoes, category }) {
       </div>
 
       {/* SEARCH */}
-      <div className={`search-container ${searchIsOpen ? "active" : ""}`}>
-        <div className="search-head">
-          <div className="search-all">
-            <img src="icons/search-all.png" alt="Search" />
-            <input
-              id="search-input"
-              type="text"
-              placeholder="Search"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-          </div>
-          <button className="cancel-search-btn" onClick={toggleSearch}>
-            Cancel
-          </button>
-        </div>
-
-        {/* SEARCH RESULTS - ONLY SHOWS UP IF THERE IS VALUE */}
-        {searchTerm && (
-          <div className="search-results">
-            <p className="top-results">Top Results</p>
-            <div className="shoes-grid">
-              {firstSixResults.map((shoe) => (
-                <div onClick={toggleSearch} key={shoe.id}>
-                  <ShoeCard key={shoe.id} shoe={shoe}></ShoeCard>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
+      <Search
+        shoes={shoes}
+        searchIsOpen={searchIsOpen}
+        toggleSearch={(toggle) => setSearchIsOpen(toggle)}
+      ></Search>
     </div>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, useRef } from "react";
+import "../styles.css";
+import ShoeCard from "./ShoeCard";
+
+export default function Search({ shoes, searchIsOpen, toggleSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchInputRef = useRef(null);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // This function checks if the shoe name or anime name matches the search term
+  const matchesSearchTerm = (shoe, searchTerm) => {
+    return (
+      shoe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      shoe.anime.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+
+  // This variable holds the filtered shoes based on the search term
+  const filteredShoes = shoes.filter((shoe) => {
+    return matchesSearchTerm(shoe, searchTerm);
+  });
+
+  // Used to limit the number of results shown in the search results
+  const firstSixResults = filteredShoes.slice(0, 8);
+
+  // This effect runs when the search is opened and focuses the input field
+  useEffect(() => {
+    if (searchIsOpen) {
+      searchInputRef.current.focus();
+    }
+  }, [searchIsOpen]);
+
+  return (
+    <div className={`search-container ${searchIsOpen ? "active" : ""}`}>
+      <div className="search-head">
+        <div className="search-all">
+          <img src="icons/search-all.png" alt="Search" />
+          <input
+            id="search-input"
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearch}
+            ref={searchInputRef}
+          />
+        </div>
+        <button
+          className="cancel-search-btn"
+          onClick={() => toggleSearch(false)}
+        >
+          Cancel
+        </button>
+      </div>
+
+      {/* SEARCH RESULTS - ONLY SHOWS UP IF THERE IS VALUE */}
+      {searchTerm && (
+        <div className="search-results">
+          <p className="top-results">Top Results</p>
+          <div className="shoes-grid">
+            {firstSixResults.map((shoe) => (
+              <div onClick={() => toggleSearch(false)} key={shoe.id}>
+                <ShoeCard key={shoe.id} shoe={shoe}></ShoeCard>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
